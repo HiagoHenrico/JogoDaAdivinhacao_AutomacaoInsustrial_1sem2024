25ff04e9f756d2af264cc27e13e244f4a5e04c15
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-08-11 20:47:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogoAcertouPerdeu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Código fonte jogo acertou perdeu;
+
+                int vez = 1, li = 1, ls = 100, palpite, qtd_jogadores;
+                 bool acertou = false;
+
+            Console.WriteLine("Bem-vindo ao Jogo do Acertou: Perdeu! - Versão Final");
+
+                Random num_aleatorio = new Random();
+                int num_oculto = num_aleatorio.Next(1, 100); 
+
+                Console.Write("Digite a quantidade de jogadores: ");
+                qtd_jogadores = int.Parse(Console.ReadLine());
+
+                while (!acertou)
+                {
+                    Console.Write($"Jogador {vez}, digite seu palpite: ");
+                    palpite = int.Parse(Console.ReadLine());
+
+                    if (palpite == num_oculto)
+                    {
+                        //Console.Write(num_oculto);
+                        Console.WriteLine($"Acertou! Jogador {vez} perdeu!");
+                        acertou = true;
+                    }else if (palpite < 1 || palpite > 100)
+                    {
+                        Console.WriteLine("Errado! O palpipe precisar se um número de 1 à 100. ");
+                        break;
+                     }
+                    else
+                    {
+                        Console.WriteLine($"Errado! Tente novamente. Jogador {vez}");
+                        vez = vez % qtd_jogadores + 1;
+                    }
+                }
+        }
+    }
+}
